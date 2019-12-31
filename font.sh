c9fee02e6e31e577139adb15a636207a66d0b85c
easy <42052283+xudazhu1@users.noreply.github.com>
2019-12-31 11:58:32 +0800

    安装宋体的字体

---
+++ font.sh
@@ -0,0 +1,10 @@
+#安装支持
+yum  -y  install  fontconfig mkfontscale ttmkfdir
+mkdir /usr/shared/fonts/chinese -pv
+wget https://github.com/xudazhu1/support/raw/master/simsun.ttc /usr/shared/fonts/chinese
+chmod -R 755 /usr/share/fonts/chinese
+#4.建立字体缓存
+cd /usr/share/fonts/chinese/
+mkfontscale
+mkfontdir
+fc-cache -fv
