aeb0fe2716b14e698b2408a3058952be74b9fbf5
easy <42052283+xudazhu1@users.noreply.github.com>
2019-12-31 14:42:52 +0800

    Update font.sh

--- font.sh
+++ font.sh
@@ -1,10 +1,10 @@
 #安装支持
 yum  -y  install  fontconfig mkfontscale ttmkfdir
-mkdir /usr/shared/fonts/chinese -p
-wget https://github.com/xudazhu1/support/raw/master/simsun.ttc /usr/shared/fonts/chinese/
-chmod -R 755 /usr/share/fonts/chinese
+mkdir /usr/shared/fonts/local/chinese -p
+wget https://github.com/xudazhu1/support/raw/master/simsun.ttc /usr/shared/fonts/local/chinese/
+chmod -R 755 /usr/share/fonts/local/chinese
 #4.建立字体缓存
-cd /usr/share/fonts/chinese/
-mkfontscale
-mkfontdir
+#cd /usr/share/fonts/local/chinese/
+mkfontscale /usr/share/fonts/local/chinese/
+mkfontdir /usr/share/fonts/local/chinese/
 fc-cache -fv
